b774170d17399b18e8826697daddc039eee2ff21
doebler <doebler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-20 20:08:04 +0000

    added xlim and ylim args to crosshair continued to write vignette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mada@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/crosshair.R
+++ pkg/R/crosshair.R
@@ -2,14 +2,15 @@
 crosshair <- function(x, ...) UseMethod("crosshair")
 
 crosshair.default <- function(x, correction = 0.5, level = 0.95, method = "wilson", 
-                      length = 0.1, pch = 1, add = FALSE, suppress = TRUE, ...){
+                      xlim = c(0,1), ylim = c(0,1), length = 0.1, pch = 1, 
+                      add = FALSE, suppress = TRUE, ...){
 if(suppress){x <- suppressWarnings(  x <- madad(x, correction = correction, level = level, 
              method = method))
              }else{
                x <-   x <- madad(x, correction = correction, level = level, 
              method = method)
              }
-  if(!add){plot(x$fpr$fpr, x$sens$sens, xlim = c(0,1), ylim =c(0,1), pch = pch, 
+  if(!add){plot(x$fpr$fpr, x$sens$sens, xlim = xlim, ylim = ylim, pch = pch, 
                 xlab = "False Positive Rate", ylab = "Sensitivity", ...)}
   if(add){points(x$fpr$fpr, x$sens$sens, pch = pch, ...)}
   arrows(x$fpr$fpr.ci[,1], x$sens$sens,  x$fpr$fpr.ci[,2], x$sens$sens, angle = 90, code = 3, length = length, ...)
